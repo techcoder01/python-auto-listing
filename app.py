ad5ccc54d7423c716fa4a203d6b440f384b30d78
Abdul Hannan <121096664+techcoder01@users.noreply.github.com>
2025-04-30 19:13:50 +0500

    Update app.py

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ import cloudinary.uploader
 import cloudinary.api
 
 cloudinary.config(
-    cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
-    api_key=os.getenv('CLOUDINARY_API_KEY'),
-    api_secret=os.getenv('CLOUDINARY_API_SECRET'),
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
     secure=True
 )
 
